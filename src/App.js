cd05d6ac71076f2e034fbe12b580a95e92344e78
Cuongtv99 <Cuongtv99@users.noreply.github.com>
2021-01-16 21:22:44 +0700

    'finish'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import "./App.css";
+import ExampleLayout from "./ExampleLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <ExampleLayout />
+    </div>
+  );
+}
+
+export default App;
